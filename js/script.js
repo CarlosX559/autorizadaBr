18dd59da9ce28c537c202af322a909aac7151f71
Carlos Xavier <carlos.lipe559@hotmail.com>
2023-03-31 12:22:04 -0300

    section faq

--- js/script.js
+++ js/script.js
@@ -18,3 +18,33 @@ function animation_scroll() {
 if (animations.length) {
   window.addEventListener("scroll", animation_scroll);
 }
+
+function faq() {
+
+  const quest = document.querySelectorAll(".faq_quests");
+  const aswer = document.querySelectorAll(".aswer");
+  const img_icon = document.querySelectorAll('.quest img');
+
+  for (let i = 0; i < quest.length; i++) {
+
+      quest[i].addEventListener("click", () => {
+
+          if (quest[i].classList.contains("close")) {
+              quest[i].classList.toggle("close");
+              aswer[i].classList.toggle("open");
+              img_icon[i].src = 'img/+.png';
+          } else {
+              quest[i].classList.add("close");
+              aswer[i].classList.add("open");
+
+              img_icon[i].src = 'img/-.png';
+          }
+
+
+      });
+
+  }
+
+
+}
+faq();
